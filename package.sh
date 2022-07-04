9ad8372e95488bc77e1d5cfe48266e3d2818b13f
xiaoxu <xiaoxu@rongcloud.cn>
2022-07-04 16:54:44 +0800

    1.修改打包脚本 2.修改xcode配置信息

--- package.sh
+++ package.sh
@@ -9,15 +9,15 @@ RCProjectName=$1
 #scheme名字 -可以点击Product->Scheme->Manager Schemes...查看
 RCScheme=$2
 #Release还是Debug
-RCConfiguration=$3
+RCConfiguration="Release"
 #日期
 #RCDate=`date +%Y%m%d_%H%M`
 #工程路径
-RCWorkspace=$4
+RCWorkspace=$3
 #build路径
-RCBuildDir=$5
+RCBuildDir=$4
 #plist文件名，默认放在工程文件路径的位置
-RCPlistName=$6
+RCPlistName=$5
 
 #创建构建和输出的路径
 mkdir -p $RCBuildDir
@@ -44,12 +44,12 @@ xcodebuild -exportArchive \
 -exportPath "$RCBuildDir/IPA" \
 -exportOptionsPlist "$RCWorkspace/$RCPlistName"
 
-open $RCBuildDir
+# open $RCBuildDir
 
 }
 
 #函数调用
 # $1 工程名  $2 scheme名字  $3 Release还是Debug  $4 工程路径  $5 ipa文件输出路径 $6 plist文件名字
-packaging "Empty" "Empty"  "Release"  "./" "./build" "./ExportOptions.plist"
+packaging "Empty" "Empty" "./" "./build" "./ExportOptions.plist"
 
 
